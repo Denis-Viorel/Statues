9af8e327479e9f04355f7fad07d5711dee7f2cf8
Denis-Viorel <61794142+Denis-Viorel@users.noreply.github.com>
2024-11-07 20:03:00 +0200

    initial commit

---
+++ Statues/Assets/Assets/Scripts/CrowdManager_SCPT.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdManager_SCPT : MonoBehaviour
+{
+    private List<Collider> collidersInContact = new List<Collider>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(collidersInContact.Count);
+    }
+
+    public void StartCrowdCoroutine()
+    {
+        StartCoroutine(CrowdCheckRoutine(0.5f));
+    }
+
+    private IEnumerator CrowdCheckRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+    
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Hit!");
+        /*Collider otherCollider = collision.GetComponent<Collider>();
+        if (!collidersInContact.Contains(otherCollider))
+        {
+            collidersInContact.Add(otherCollider);
+        }*/
+    }
+
+}
