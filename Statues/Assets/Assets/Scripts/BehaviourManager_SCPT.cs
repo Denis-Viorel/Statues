dcf50dde0caa1c2d2f49990f92cf43c5e29e70d0
ElenaFaur <faurelena2000@yahoo.com>
2024-11-23 16:41:37 +0200

    added chance, delay, setup

--- Statues/Assets/Assets/Scripts/BehaviourManager_SCPT.cs
+++ Statues/Assets/Assets/Scripts/BehaviourManager_SCPT.cs
@@ -1,31 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
  public enum AgentType{
-        Normie,
         Protector,
-        Saboteur
+        Saboteur,
+        Normie
     }
 
 public class BehaviourManager_SCPT : MonoBehaviour
 {
     AgentType type = new AgentType();
     [SerializeField]CrowdManager_SCPT crowdManager;
+    [SerializeField]GlobalManager_SCPT globalManager;
 
     private int calm;
+    private int calmMin;
+    private int panic;
+    private float chanceProtector = 0.10f;
+    private float chanceSaboteur = 0.10f;
+    private float typeRoll;
+    private float time;
+    private bool isPanicked = false;
     // Start is called before the first frame update
     void Start()
     {
-        calm = Random.Range(80, 100);
-        type = (AgentType)Random.Range(0, System.Enum.GetValues(typeof(AgentType)).Length);
-        CrowdManager_SCPT crowdManager = GetComponentInChildren<CrowdManager_SCPT>();    }
+        calm = Random.Range(calmMin, 100);
+        // calm = 1;
+        SetTypeAgent();
+        CrowdManager_SCPT crowdManager = GetComponentInChildren<CrowdManager_SCPT>();   
+        time = Time.time; 
+    }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Type: " + type);
-        crowdManager.AgentCrowdEffect( calm, type);
+        
     }
 
     public int GetCalm(){
@@ -37,14 +49,6 @@ public class BehaviourManager_SCPT : MonoBehaviour
         Debug.Log("Valori initiale: " + "tip agent primit: " + typeReceiving);
         Debug.Log("Valori initiale: " + "calm: " + calm);
         switch ( type ){
-            case AgentType.Normie:{
-                if( calm < value )
-                    calm ++;
-                else if( calm > value && typeReceiving != AgentType.Protector )
-                        calm--;
-                Debug.Log("Normie:" + calm );
-                 break;
-            }
             case AgentType.Protector:{
                  if( calm < value && typeReceiving != AgentType.Saboteur )
                     calm ++;
@@ -57,7 +61,59 @@ public class BehaviourManager_SCPT : MonoBehaviour
                 Debug.Log("Saboteur:" + calm );
                 break;
             }
+            case AgentType.Normie:{
+                if( calm < value )
+                    calm ++;
+                else if( calm > value && typeReceiving != AgentType.Protector )
+                        calm--;
+                Debug.Log("Normie:" + calm );
+                 break;
+            }
         }
         Debug.Log("Dupa switch: " + calm);
+    }
+
+    void SetTypeAgent(){
+
+        if( globalManager != null ){
+            chanceProtector = globalManager.chanceProtector;
+            chanceSaboteur = globalManager.chanceSaboteur;
+            panic = globalManager.panic;
+            calmMin = globalManager.calmMin;
+        }
+
+        typeRoll = Random.value; // returns numbers between {0 - 1}
+
+        if( typeRoll < chanceProtector ){
+            type = AgentType.Protector;
+        }
+        else if( typeRoll < chanceProtector + chanceSaboteur ){
+            type = AgentType.Saboteur;
+        }
+        else{
+            type = AgentType.Normie;
+        }
+    }
+
+    void PanicCheck(){
+         if( panic > calm ){
+            isPanicked = true;
+            Death();
+            Debug.Log("e panicat: " + isPanicked);
+         }
+    }
+
+    void Death(){
+        crowdManager.enabled = false;
+        this.enabled = false;
+    }
+
+    void FixedUpdate(){
+        if( time <= Time.time ){
+            time = Time.time + 1f;
+            Debug.Log("time: " + time);
+            crowdManager.AgentCrowdEffect( calm, type );
+            PanicCheck();
+        }
     }
 }
--- Statues/Assets/Assets/Scripts/GlobalManager_SCPT.cs
+++ Statues/Assets/Assets/Scripts/GlobalManager_SCPT.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GlobalManager_SCPT : MonoBehaviour
 {
+    [SerializeField] public float chanceProtector = 0.20f;
+    [SerializeField] public float chanceSaboteur = 0.20f;
+    [SerializeField] public int panic = 50;
+    [SerializeField] public int calmMin = 80;
     // Start is called before the first frame update
     void Start()
     {
