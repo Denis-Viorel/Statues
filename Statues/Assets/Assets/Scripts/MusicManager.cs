99c24848b142564b8700cabd1d42f771d00607e2
Denis-Viorel <61794142+Denis-Viorel@users.noreply.github.com>
2025-02-01 21:39:17 +0200

    Pitch and Volume Update
    Pitch Decrease and Volume increases as calm goes down

--- Statues/Assets/Assets/Scripts/MusicManager.cs
+++ Statues/Assets/Assets/Scripts/MusicManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] private bool isInGameScene;
+    [SerializeField] private GlobalManager_SCPT globalManager;
+    [SerializeField] private AudioSource bgMusic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isInGameScene = false;
     }
 
     private void Awake()
@@ -18,6 +24,20 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(SceneManager.GetActiveScene().name.Equals("SampleScene") && (isInGameScene == false))
+        {
+            isInGameScene = true;
+            globalManager = GameObject.Find("GlobalManager").GetComponent<GlobalManager_SCPT>();
+        }
+        if(isInGameScene == true)
+        {
+            float calmValue = globalManager.calmGlobal;
+
+            float effectiveCalm = Mathf.Max(calmValue, 25f);
+
+            bgMusic.pitch = Mathf.Lerp(-3f, 1f, (effectiveCalm - 25f) / (100f - 25f));
+
+            bgMusic.volume = Mathf.Lerp(1.0f, 0.4f, (effectiveCalm - 25f) / (100f - 25f));
+        }
     }
 }
